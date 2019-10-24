fd970babe758a139375645f2d04e4e5ff55f1d98
jeanshanchik <kelleyshann@gmail.com>
2019-10-24 09:01:08 -0400

    Task: Added functions for calculator

---
+++ src/Calculator.py
@@ -0,0 +1,25 @@
+import math
+
+def addition(a, b):
+    c = a + b
+    return c
+
+def subtraction(a, b):
+    c = a - b
+    return c
+
+def multiplication(a, b):
+    c = a * b
+    return c
+
+def division(a, b):
+    c = a/b
+    return c
+
+def square(a, b):
+    c = a ** b
+    return c
+
+def squareroot(a):
+    sqrt_a = math.sqrt(a)
+    return(sqrt_a)
