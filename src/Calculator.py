afa08ae9b5a92171aa0e7a9c82497eaf38fa7f87
jeanshanchik <kelleyshann@gmail.com>
2019-10-24 09:12:54 -0400

    Feature: added calculator class object

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,4 +1,8 @@
+#importing math module for squareroot function
+
 import math
+
+#defining calculator functions
 
 def addition(a, b):
     c = a + b
@@ -23,3 +27,36 @@ def square(a, b):
 def squareroot(a):
     sqrt_a = math.sqrt(a)
     return(sqrt_a)
+
+#instantiating Calculator class
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+    
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+    
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def squaring(self, a, b):
+        self.result = square(a, b)
+        return self.result
+    
+    def takesqrroot(self, a, b):
+        self.result = squareroot(a, b)
+        return self.result
+
