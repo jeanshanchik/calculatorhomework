762ec72097349a67f2e25380db52e15b2607cc22
Kelley Shann <kelleyshann@gmail.com>
2019-10-26 18:39:25 -0400

    Fix: Rewrote code for csvreader module

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,36 +1,12 @@
-# importing math module for squareroot function
-
-import math
-
-
-# defining calculator functions
-
 def addition(a, b):
-    return a + b
+    c = a + b
+    return c
 
 
 def subtraction(a, b):
-    return a - b
+    c = a - b
+    return c
 
-
-def multiplication(a, b):
-    return a * b
-
-
-def division(a, b):
-    return a / b
-
-
-def square(a):
-    return a**2
-
-
-def squareroot(a):
-    sqrt_a = math.sqrt(a)
-    return sqrt_a
-
-
-# instantiating Calculator class
 
 class Calculator:
     result = 0
@@ -39,25 +15,9 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
 
     def subtract(self, a, b):
-        self.result = a - b
-        return subtraction(a, b)
-
-    def multiply(self, a, b):
-        self.result = a * b
-        return multiplication(a, b)
-
-    def divide(self, a, b):
-        self.result = a / b
-        return division(a, b)
-
-    def squaring(self, a):
-        self.result = a**2
-        return square(a)
-
-    def takesqrroot(self, a):
-        self.result = squareroot(a)
+        self.result = subtraction(a, b)
         return self.result
