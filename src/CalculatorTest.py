5000e379a3b789a457dff8e36e2a166d5f1ac05c
Kelley Shann <kelleyshann@gmail.com>
2019-10-26 18:38:21 -0400

    Fix: Rewrote code for tests and calculator module

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -3,40 +3,18 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
-    def test_add_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+    def test_subtraction(self):
+        self.assertEqual(self.calculator.subtract(1, 1), 0)
+        self.assertEqual(self.calculator.result, 0)
 
-    def test_sub_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2, 2), 0)
-        self.assertEqual(calculator.result, 0)
-
-    def test_mult_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.multiply(2, 5), 10)
-        self.assertEqual(calculator.result, 10)
-
-    def test_divide_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.divide(10, 2), 5)
-        self.assertEqual(calculator.result, 5)
-
-    def test_square_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.squaring(5), 25)
-        self.assertEqual(calculator.result, 25)
-
-    def test_sqrt_method(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.takesqrroot(25), 5)
-        self.assertEqual(calculator.result, 5)
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ == '__main__':
